0349d58a0620588f849ad5320560d9d5a9c8530e
Christine Xiang <yutingcxiang@gmail.com>
2018-06-15 05:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(katzDeli) {
-  
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  else {
+      
+    }
+  }
 }
