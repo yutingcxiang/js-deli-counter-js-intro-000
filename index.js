7dda70ac0683101ebe4b5faa233f5427c556e324
Christine Xiang <yutingcxiang@gmail.com>
2018-06-15 05:42:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (var i in katzDeli) {
+    for (var i = 0; i< katzDeli.length; i++) {
       line.push(`${i+1}. ${katzDeli[i]} `);
     }
     return "The line is currently: " + line;
